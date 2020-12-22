f23d6907052647110ac44701889b8ffa446557a6
toño <joseantonioaraujodemiguel@gmail.com>
2020-12-22 19:26:15 +0100

    añado parte de los componentes y routing de detalle de alquileres

---
+++ src/app/alquileres/alquileres-detail/alquileres-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alquileres-detail',
+  templateUrl: './alquileres-detail.component.html',
+  styleUrls: ['./alquileres-detail.component.css']
+})
+export class AlquileresDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/alquileres/alquileres.module.ts
+++ src/app/alquileres/alquileres.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AlquileresRoutingModule } from './alquileres-routing.module';
 import { AlquileresHomeComponent } from './alquileres-home/alquileres-home.component';
+import { AlquileresDetailComponent } from './alquileres-detail/alquileres-detail.component';
 
 
 @NgModule({
 
   declarations: [
-    AlquileresHomeComponent
+    AlquileresHomeComponent,
+    AlquileresDetailComponent
   ],
 
   imports: [
